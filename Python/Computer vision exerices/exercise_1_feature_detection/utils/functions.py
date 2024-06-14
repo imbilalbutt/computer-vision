90ba89fe17cb46779eb7847cb7ad720a13cc7eda
Butt <eg87oqic@fauad.fau.de>
2024-06-14 04:23:34 +0200

    ex1 almost done

--- Python/Computer vision exerices/exercise_1_feature_detection/utils/functions.py
+++ Python/Computer vision exerices/exercise_1_feature_detection/utils/functions.py
@@ -42,8 +42,8 @@ def compute_harris_response(I: np.array, k: float = 0.06) -> Tuple[np.array]:
 
     # Step 4: Compute the harris response with the determinant and the trace of T
     # Method: 1
-    # Wdet = Ixx * Iyy - Ixy ** 2
-    # Wtr = Ixx + Iyy
+    # Wdet = Ixx * Iyy - Ixy ** 2 # A * B - C ** 2
+    # Wtr = Ixx + Iyy # A + B
     # R = Wdet / Wtr
 
     # Method 2
@@ -56,7 +56,6 @@ def compute_harris_response(I: np.array, k: float = 0.06) -> Tuple[np.array]:
     # T = np.array([A, C])
     # T = np.append(T, [C, B], axis=0)
     # R = np.linalg.det(T) - (k * np.trace(T) ^ 2)
-
 
     return R, A, B, C, Idx, Idy
 
@@ -74,16 +73,47 @@ def detect_corners(R: np.array, threshold: float = 0.1) -> Tuple[np.array, np.ar
         A tuple of two 1D integer arrays containing the x and y coordinates of key-points in the image.
     """
     # Step 1 (recommended): Pad the response image to facilitate vectorization
+    padded_image = np.pad(R, 1, mode='constant')
 
     # Step 2 (recommended): Create one image for every offset in the 3x3 neighborhood
+    # offset = 3
+    # result = np.zeros([padded_R.shape[0] - (offset - 1), padded_R.shape[1] - (offset - 1)])
+    #
+    # # Iterate over the box filter
+    # for filter_y in range(offset):
+    #     for filter_x in range(offset):
+    #         # Sum up the values covered by the filter in the result image
+    #         result += padded_R[filter_y:filter_y + result.shape[0],
+    #                   filter_x:filter_x + result.shape[1]]
+
+    # offset_images = np.zeros((R.shape[0], R.shape[1]))
+    # for filter_y in range(offset):
+    #     for filter_x in range(offset):
+    #         pass
+
+    offsets = [
+        padded_image[0:-2, 0:-2],  # Top-left
+        padded_image[0:-2, 1:-1],  # Top
+        padded_image[0:-2, 2:],    # Top-right
+        padded_image[1:-1, 0:-2],  # Left
+        padded_image[1:-1, 2:],    # Right
+        padded_image[2:, 0:-2],    # Bottom-left
+        padded_image[2:, 1:-1],    # Bottom
+        padded_image[2:, 2:]       # Bottom-right
+    ]
 
     # Step 3 (recommended): Compute the greatest neighbor of every pixel
+    # greatest_neighbors = np.amax(result, axis=0)
+    # np.max(H(:))
+    max_neighbors = np.maximum.reduce(offsets)
 
     # Step 4 (recommended): Compute a boolean image with only all key-points set to True
+    keypoints = (R > max_neighbors) & (R > threshold)
 
     # Step 5 (recommended): Use np.nonzero to compute the locations of the key-points from the boolean image
+    points = np.nonzero(keypoints)
 
-    raise NotImplementedError
+    return points
 
 
 def detect_edges(R: np.array, edge_threshold: float = -0.01) -> np.array:
